f97cf7449f3d75bfd0a9b0615cce0271e507e7f8
russelibale808 <russelibale808@gmail.com>
2022-11-23 21:35:24 -1000

    ASPNETExercise

---
+++ Testing/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Testing.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryID { get; set; }
+        public bool OnSale { get; set; }
+        public string StockLevel { get; set; }
+    }
+}
